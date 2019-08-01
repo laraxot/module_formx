e335886ee586734c7bf805397a8dc385e9c35e3a
marco76tv <marco.sottana@gmail.com>
2019-08-01 09:54:18 +0200

    .

--- Resources/views/includes/components/btn/destroy.blade.php
+++ Resources/views/includes/components/btn/destroy.blade.php
@@ -4,10 +4,28 @@
 	Theme::add('theme/bc/sweetalert2/dist/sweetalert2.min.css');
 	Theme::add('extend::js/btnDeleteX2.js'); 
 	*/
-@endphp
+	//DestructiveAction
+/*
 <a class="{{ $btn_class }}" href="#" data-token="{{ csrf_token() }}" data-id="{{ $id }}" data-href="{{ $route }}" data-toggle="tooltip" title="Cancella">
-	{{--      
-	<i class="fa fas fa-trash-alt fa-trash-o fa-fw" aria-hidden="true"></i>
-	--}}
 	<i class="far fa-trash-alt"></i>
+</a>
+*/
+@endphp
+@php
+	$tag_attr=[
+		'class'=>$btn_class.' btn-confirm-delete btn-danger',
+		'href'=>"#",
+		'data-token'=>csrf_token(),
+		'data-id'=>$id,
+		'data-href'=>$route,
+		'data-toggle'=>"tooltip",
+		'title'=>"Destroy",
+	];
+	//ddd(http_build_query($tag_attr, '', ' '));//no
+	$tag_attr=collect($tag_attr)->map(function($v,$k){
+		return $k.'="'.$v.'"';
+	})->implode(' ');
+@endphp
+<a {!! $tag_attr !!}>
+	<i class="far fa-trash-alt"></i>{{-- $route --}}
 </a>
