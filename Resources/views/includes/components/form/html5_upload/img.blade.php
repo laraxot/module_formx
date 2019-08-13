bd3f811f18ec2d585aba00464c8bc9bb424b85ce
marco76tv <marco.sottana@gmail.com>
2019-08-13 22:59:17 +0200

    .

--- Resources/views/includes/components/form/html5_upload/img.blade.php
+++ Resources/views/includes/components/form/html5_upload/img.blade.php
@@ -1,15 +1,16 @@
 @php
-	Theme::add($comp_view.'/js/html5imageupload.js');
-	Theme::add($comp_view.'/css/html5imageupload.css');
-	Theme::add($comp_view.'/css/glyphicons.css');
-	Theme::add($comp_view.'/css/style.css'); 
+	//Theme::add($comp_view.'/js/html5imageupload.js');
+	//Theme::add($comp_view.'/css/html5imageupload.css');
+	//Theme::add($comp_view.'/css/glyphicons.css');
+	//Theme::add($comp_view.'/css/style.css'); 
+
 	$label=isset($attributes['label'])?$attributes['label']:trans($view.'.field.'.$name);
 	$placeholder=isset($attributes['placeholder'])?$attributes['placeholder']:trans($view.'.field.'.$name.'_placeholder');
 @endphp
 
-<div class="form-group">
-    <label>{{ $label }}</label>
-    <div class="dropzone" data-width="400" data-height="400" data-url="{{-- route('imgz.canvas') --}}"  style="width: 100%;" data-image="{{ $value }}">
+<div class="form-group"> 
+    <label>{{ $label }}</label> {{-- url('it/image?_act=canvas') --}}
+    <div class="dropzone" data-width="400" data-height="400" data-url="{{ url('it/image?_act=canvas') }}"  style="width: 100%;" data-image="{{ $value }}">
      	{{ Form::file($name.'_thumb', $value, array_merge(['id'=>$name.'_thumb', 'class' => 'form-control'], $attributes)) }}
 	</div>
 	{{ Form::text($name, $value, array_merge(['id'=>$name,'class' => 'form-control','placeholder'=>$placeholder], $attributes)) }}
@@ -19,9 +20,10 @@
 		</span>
 	@endif
 </div>
+{{--
 @push('scripts')
 <script>
-$(function () {
+//$(function () {
 	$.ajaxSetup({
     	headers: {
         	'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -29,13 +31,16 @@ $(function () {
 	});
 	$('.dropzone').html5imageupload({
 		onAfterProcessImage: function() {
+			alert('onAfterProcessImage');
 			var $val=$(this.element).data('name');
 			$('#{{ $name }}').val($val);
 		},
 		onAfterCancel: function() {
+			alert('onAfterCancel');
 			$('#{{ $name }}').val('');
 		}
-});
-});
+	});
+//});
 </script>
 @endpush	    
+--}}
