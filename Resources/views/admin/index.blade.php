8426aa2ab2b8c9edc917218c32c48a1a07159d97
marco76tv <marco.sottana@gmail.com>
2019-08-27 14:36:15 +0200

    .

---
+++ Resources/views/admin/index.blade.php
@@ -0,0 +1,14 @@
+@extends('adm_theme::layouts.app')
+@section('content')
+	
+  
+  	example-component[<example-component></example-component>]
+  	date-component[<date-component></date-component>]
+  	rating comp[<rating-component></rating-component>]
+    <v-row justify="center">
+      <v-date-picker ></v-date-picker>
+    </v-row>
+  
+
+
+@endsection
