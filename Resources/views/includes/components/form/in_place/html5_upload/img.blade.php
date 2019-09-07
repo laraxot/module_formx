03d798bb4c6e338e87385441fc1ceeb7837ec067
marco76tv <marco.sottana@gmail.com>
2019-09-08 01:06:13 +0200

    .

--- Resources/views/includes/components/form/in_place/html5_upload/img.blade.php
+++ Resources/views/includes/components/form/in_place/html5_upload/img.blade.php
@@ -3,12 +3,12 @@
 	$placeholder=isset($attributes['placeholder'])?$attributes['placeholder']:trans($view.'.field.'.$name.'_placeholder');
 	$val=Form::getValueAttribute($name);
 	$model=Form::getModel();
-
+	//$canvas_url=route('imgz.canvas');
 @endphp
 
 <div class="form-group">
     <label>{{ $label }}</label>
-    <div class="dropzone" data-width="400" data-height="400" data-url="{{ route('imgz.canvas') }}"  style="width: 100%;" data-image="{{ $val }}" data-field="{{ $name }}" data-updateurl="{{ $model->update_url }}" data-name="{{ $name }}">
+    <div class="dropzone" data-width="400" data-height="400" data-url="{{ $canvas_url }}"  style="width: 100%;" data-image="{{ $val }}" data-field="{{ $name }}" data-updateurl="{{ $model->update_url }}" data-name="{{ $name }}">
      	{{ Form::file($name.'_thumb', $value, array_merge(['id'=>$name.'_thumb', 'class' => 'form-control'], $attributes)) }}
 	</div>
 	{{ Form::text($name, $value, array_merge(['id'=>$name,'class' => 'form-control','placeholder'=>$placeholder], $attributes)) }}
