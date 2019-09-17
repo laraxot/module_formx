3a3f98a409e6b773f133ad437e846cc0047e5cdc
marco76tv <marco.sottana@gmail.com>
2019-09-17 16:47:29 +0200

    .

--- Resources/views/includes/components/form/2_password.blade.php
+++ Resources/views/includes/components/form/2_password.blade.php
@@ -1,36 +1,13 @@
 @php
-	/*
-	if(isset($attributes['label']))
-		$label=$attributes['label'];
-	else
-		$label=trans($view.'.field.'.$name);
-	*/
 	$field=transFields(get_defined_vars());
-	if(Str::endsWith($name,']')){
-		$name_conf=substr($name,0,-1).'_confirmation]';
-	}else{
-		$name_conf=$name.'_confirmation';
-	}
+	$name_conf=bracketsToDotted($name).'_confirmation';
+	$name_conf=dottedToBrackets($name_conf);
 @endphp
 <div class="row">
 <div class="col-sm-6">
-@component($blade_component,get_defined_vars())
-	@slot('label')
-		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
-	@endslot
-	@slot('input')
-		{{ Form::password($name, $field->attributes) }}
-	@endslot
-@endcomponent
+	{{ Form::bsPassword($name) }}
 </div>
 <div class="col-sm-6">
-@component($blade_component,get_defined_vars())
-	@slot('label')
-		{{ Form::label($name_conf, $field->label , ['class' => 'control-label']) }}
-	@endslot
-	@slot('input')
-		{{ Form::password($name_conf.'__confirmation', $field->attributes) }}
-	@endslot
-@endcomponent
+	{{ Form::bsPassword($name_conf) }}
 </div>
 </div>
