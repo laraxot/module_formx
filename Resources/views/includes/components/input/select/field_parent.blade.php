1d46d1f86c35e683c3c7555cfe7d1e40bbed0093
marco76tv <marco.sottana@gmail.com>
2019-11-19 14:32:41 +0100

    .

--- Resources/views/includes/components/input/select/field_parent.blade.php
+++ Resources/views/includes/components/input/select/field_parent.blade.php
@@ -1,12 +1,40 @@
 @php
 	$options=[];
 	extract($attributes);
-	ddd(get_defined_vars());
+	//ddd(get_defined_vars());
 	$field=transFields(get_defined_vars());
 	$row=Form::getModel();
 	$row_panel=Panel::get($row);
 	//ddd($row_panel);
 	//ddd($row_panel->rows([]) );
+	$options=$row->get()->map(function($item) use ($row_panel){
+		return [
+			'id'=>$row_panel->optionId($item),
+			'parent_id'=>$item->parent_id,
+			'title'=>$row_panel->optionLabel($item),
+		];
+	})->groupBy('parent_id')
+	->all()
+	;
+
+	$tmp=[];
+	foreach($options[0] as $root){
+		$root['title']='-- '.$root['title'];
+		$tmp[]=$root;
+		$sons=isset($options[$root['id']])?$options[$root['id']]:[];
+		foreach($sons as $son){
+			$son['title']='---- '.$son['title'];
+			$tmp[]=$son;
+		}
+	}
+	/*
+	->pluck('title','id')
+	->prepend('Root',0)
+	->all();
+	*/
+	//$options[0]='Root';
+	$options=collect($tmp)->pluck('title','id')->prepend('Root',0);
+
 @endphp
 
 @component($blade_component,get_defined_vars())
