242741952e1c147ab4858c584b4216d0f9439b44
marco76tv <marco.sottana@gmail.com>
2019-11-19 21:51:21 +0100

    .

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 require('laravel-mix-merge-manifest');
 
-mix.setPublicPath('../../public').mergeManifest();
+mix.setPublicPath('../../../public_html').mergeManifest();
 
 mix.js(__dirname + '/Resources/assets/js/app.js', 'js/formx.js')
     .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/formx.css');
