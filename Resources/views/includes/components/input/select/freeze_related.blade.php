58e3e4273493fbba38abda87112e13be2b27f4f1
marco76tv <marco.sottana@gmail.com>
2019-11-20 10:16:21 +0100

    .

--- Resources/views/includes/components/input/select/freeze_related.blade.php
+++ Resources/views/includes/components/input/select/freeze_related.blade.php
@@ -1,5 +1,16 @@
 @php
 	$class=$field->related_class;
-	$obj=$class::find($field->value);
+	if(class_exists($class)){
+		$obj=$class::find($field->value);
+		if(!is_object($obj)){
+			$obj=new \stdClass();
+			$obj->nome='Not Exists ['.$class.']['.$field->value.']';
+		}
+	}else{
+		$obj=new \stdClass();
+		$obj->nome='Not Exists ['.$class.']';
+	}
+
+
 @endphp
 {{ $field->value }}]{{ $obj->nome }}
