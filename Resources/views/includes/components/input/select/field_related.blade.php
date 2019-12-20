367e1ab18c09a85283e9e114bdd824e3f15f66ce
marco76tv <marco.sottana@gmail.com>
2019-12-20 15:59:15 +0100

    .

--- Resources/views/includes/components/input/select/field_related.blade.php
+++ Resources/views/includes/components/input/select/field_related.blade.php
@@ -1,10 +1,16 @@
 @php
 	$related_class=($options['field']->related_class);
+	$panel_related=Panel::get(new $related_class);
+	$rows=with(new $related_class)->get();
+	$panel_related->setRows($rows);
 	//ddd(get_defined_vars());
 	$field=transFields(get_defined_vars());
 	//Category::pluck('name', 'id')
 	//$opts=['1'=>'test','2'=>'prova'];
-	$opts=$related_class::pluck('nome','id');
+
+	//$opts=$related_class::pluck('nome','id');
+	$opts=$panel_related->optionsSelect();
+
 @endphp
 @component($blade_component,get_defined_vars())
 	@slot('label')
