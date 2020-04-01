f20b6ce8b6505fe67d3b08bfe92b2c012e2fbc63
marco76tv <marco.sottana@gmail.com>
2020-04-01 10:05:16 +0200

    .

--- Resources/views/includes/components/input/email/field.blade.php
+++ Resources/views/includes/components/input/email/field.blade.php
@@ -6,6 +6,6 @@
 		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
 	@endslot
 	@slot('input')
-		{{ Form::text($name, $value, $field->attributes) }}
+		{{ Form::email($name, $value, $field->attributes) }}
 	@endslot
 @endcomponent
