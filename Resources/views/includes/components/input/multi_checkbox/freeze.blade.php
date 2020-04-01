e3c417f3b4ea923bcb88178e39e44b7d2f8d921e
marco76tv <marco.sottana@gmail.com>
2020-04-01 12:19:49 +0200

    .

--- Resources/views/includes/components/input/multi_checkbox/freeze.blade.php
+++ Resources/views/includes/components/input/multi_checkbox/freeze.blade.php
@@ -1,1 +1,7 @@
-{{ $field->value }}
+@php
+    //ddd(get_defined_vars());
+@endphp
+{{-- $field->value --}}
+@foreach($rows as $k=>$v)
+    <span class="badge badge-secondary">{{ $v->{$related_fields[1]->name} }}</span>
+@endforeach
