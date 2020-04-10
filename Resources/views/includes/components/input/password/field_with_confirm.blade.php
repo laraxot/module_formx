095a4de2e7bf339934b4617b15169620a227eaf8
Malebestia <malebestia@yahoo.it>
2020-04-10 13:15:45 +0200

    .

--- Resources/views/includes/components/input/password/field_with_confirm.blade.php
+++ Resources/views/includes/components/input/password/field_with_confirm.blade.php
@@ -2,14 +2,27 @@
 	$field=transFields(get_defined_vars());
 	$name_conf=bracketsToDotted($name).'_confirmation';
 	$name_conf=dottedToBrackets($name_conf);
+	$without_div=false;
+	if(isset($attributes['without_div'])){
+		$without_div=$attributes['without_div'];
+	}
 @endphp
+@if($without_div)
+@else
 <div class="row col-md-12" >
 	<div class="col-md-6">
-	{{ Form::bsPassword($name) }}
+@endif
+		{{ Form::bsPassword($name) }}
+@if($without_div)
+@else
 </div>
 <div class="col-md-6">
+@endif
 	{{ Form::bsPassword($name_conf) }}
-</div>
-</div>
+@if($without_div)
+@else
 
+</div>
+</div>
+@endif
 	
