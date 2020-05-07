1c51db4999e355ffc148a157a079efbceeaa2e99
Malebestia <malebestia@yahoo.it>
2020-05-07 18:48:37 +0200

    .

--- Resources/views/includes/components/input/radio/field.blade.php
+++ Resources/views/includes/components/input/radio/field.blade.php
@@ -6,6 +6,6 @@
 		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
 	@endslot
 	@slot('input')
-		{{ Form::text($name, $value, $field->attributes) }}
+		{{ Form::radio($name, $value, $field->attributes) }}
 	@endslot
 @endcomponent
