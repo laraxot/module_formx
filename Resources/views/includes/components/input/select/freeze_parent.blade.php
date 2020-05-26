1f2fafe2fb865343c835cb29c501e823c721fc8d
Malebestia <malebestia@yahoo.it>
2020-05-26 20:04:46 +0200

    .

--- Resources/views/includes/components/input/select/freeze_parent.blade.php
+++ Resources/views/includes/components/input/select/freeze_parent.blade.php
@@ -2,7 +2,8 @@
 	$field->title='ROOT';
 	if($field->value!=0){
 		$class=get_class($row);
-		$r=$class::where('post_id',$field->value)->first();
+		//$r=$class::where('id',$field->value)->first();
+		$r=$class::find($field->value);
 		$field->title='-- sconosciuto o cancellato --';
 		if(is_object($r)){
 			$field->title=$r->title;
