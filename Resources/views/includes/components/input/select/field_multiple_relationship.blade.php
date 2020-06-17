87ec86ac4453db459d5150aa0a810d945208ccc1
Malebestia <malebestia@yahoo.it>
2020-06-17 18:31:22 +0200

    .

--- Resources/views/includes/components/input/select/field_multiple_relationship.blade.php
+++ Resources/views/includes/components/input/select/field_multiple_relationship.blade.php
@@ -9,9 +9,24 @@
     $rows=$related->get()->load('post');
     $related_panel->setRows($rows);
     $opts=$related_panel->optionsSelect();
-    
+    //dddx($opts);
     $field=transFields(get_defined_vars());
     $field->attributes['multiple']=true;
 @endphp
 
-{{ Form::select($name.'[id]', $opts,$value,$field->attributes) }}
+{{-- Form::select($name.'[id]',$opts,$value,$field->attributes) --}}
+
+<select class="js-example-basic-single" name="state">
+  <option value="AL">Alabama</option>
+    ...
+  <option value="WY">Wyoming</option>
+</select>
+
+@push('script')
+<script>
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
+</script>
+@endpush
