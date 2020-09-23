643c34239b1580595e18efc8e2c3cbfd9c5fcd69
marco76tv <marco.sottana@gmail.com>
2020-09-23 12:45:17 +0200

    .

--- Http/Livewire/Datagrid.php
+++ Http/Livewire/Datagrid.php
@@ -17,21 +17,51 @@ use Modules\Theme\Services\ThemeService;
  */
 class Datagrid extends Component {
     use WithPagination;
-    //public $containers;
-    //public $items;
-    //public $index_fields;
-    //public $rows;
+    public $index_fields = []; //private lo perde,protected anche
+    public $sql = '';
+    public $where = '';
+
     /**
      * Livewire component's [modules.form-x.http.livewire.datagrid] public property [rows] must be of type: [numeric, string, array, null, or boolean]. Only protected or private properties can be set as other types because JavaScript doesn't need to access them.
      */
-    protected $rows;
-    /*
     public function mount($_panel) {
-        //[$this->containers,$this->items] = params2ContainerItem();
-        $this->index_fields = $_panel->indexFields();
-        $this->rows = $_panel->rows->paginate(10);
+        $index_fields = $_panel->indexFields();
+        $this->index_fields = $index_fields;
+        $rows = $_panel->rows();
+        $sql = $rows->toSql();
+        $bindings = collect($rows->getBindings())->map(function ($item) {
+            return "'".$item."'";
+        })->all();
+        $this->sql = str_replace(explode(',', str_repeat('?,', 10)), $bindings, $sql);
+        if (Str::contains($this->sql, ' where ')) {
+            $this->where = Str::after($this->sql, 'where');
+        }
     }
-    */
+
+    public function printf_array($format, $arr) {
+        return call_user_func_array('printf', array_merge((array) $format, $arr));
+    }
+
+    public function render() {
+        $view = 'formx::livewire.datagrid';
+        $model = new User();
+        //newQuery(); Builder $query
+        $rows = $model->newQuery();
+        //$rows = $model; //perche' usare newQuery ?
+        //$rows = $rows->selectRaw($this->sql);
+        //*
+        if ('' != $this->where) {
+            $rows = $rows->whereRaw($this->where);
+        }
+        //*/
+        $rows = $rows->paginate(10);
+
+        $view_params = [
+            'rows' => $rows,
+        ];
+
+        return view($view, $view_params);
+    }
 
     public function getView() { //no di themeservice, perche' livewire
         $mod_name = Str::between(get_class(), 'Modules\\', '\\Http\\');
@@ -42,18 +72,5 @@ class Datagrid extends Component {
         $view = $mod_name_low.'::livewire.'.$name;
 
         return $view;
-    }
-
-    public function render() {
-        // ThemeService::getView(); //lu::admin.user.index.acts.test_users_with_livewire
-        //get_class  = Modules\FormX\Http\Livewire\Datagrid
-        //$view = $this->getView();
-        $view = 'formx::livewire.datagrid';
-        //dddx($view);
-        $view_params = [
-            'rows' => User::paginate(10), //$this->rows,
-        ];
-
-        return view($view, $view_params);
     }
 }
--- Resources/views/livewire/datagrid.blade.php
+++ Resources/views/livewire/datagrid.blade.php
@@ -1,24 +1,34 @@
 <div>
     <h3>datagrid</h3>
     <table class="table table-bordered">
-        @php
-           // print_r($index_fields);
-        @endphp
-        {{--
         <tr>
-        @foreach($index_fields as $field)
-        <th>{{ $field->name }}</th>
-        @endforeach
+            <thead>
+            @foreach($index_fields as $field)
+                @php
+                    if(!is_object($field)){
+                        $field=(object)$field;
+                    }
+                @endphp
+                    <th>{{ $field->name }}</th>
+            @endforeach
+            </thead>
+            <tbody>
+                @foreach($rows as $row)
+                <tr>
+                @foreach($index_fields as $field)
+                @php
+                    if(!is_object($field)){
+                        $field=(object)$field;
+                    }
+                @endphp
+                    <td>{{-- $row->$field->name --}}
+                        {!! Theme::inputFreeze(['field'=>$field,'row'=>$row]) !!}
+                    </td>
+                @endforeach
+                </tr>
+                @endforeach
+            </tbody>
         </tr>
-        --}}
-
-        @foreach($rows as $row)
-        <tr>
-            <td>{{ $row->auth_user_id }}</td>
-        </tr>
-        @endforeach
-        {{--
-        --}}
     </table>
      {{ $rows->links() }}
 </div>
