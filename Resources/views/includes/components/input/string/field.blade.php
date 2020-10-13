fb7ec1262251fe1737a7bcdf6e11a4d8818f8a9f
marco76tv <marco.sottana@gmail.com>
2020-10-13 11:01:03 +0200

    .

--- Resources/views/includes/components/input/string/field.blade.php
+++ Resources/views/includes/components/input/string/field.blade.php
@@ -1,18 +1,14 @@
 @php
+//dddx(get_defined_vars());
 $field=transFields(get_defined_vars());
 //dddx($field);
-//$field->attributes['wire:model']='row.'.$name;
-//$field->attributes['id']=null;
-//unset($field->attributes['id']);
+//$field->attributes['wire:model']=$name;
 @endphp
 @component($blade_component, get_defined_vars())
     @slot('label')
-        {{ Form::label($name, $field->label, ['class' => 'control-label form-label']) }}
+        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
     @endslot
     @slot('input')
-        <input placeholder="type" name="type" wire:model="row.type" type="text" class="form-control">
-        {{--
         {{ Form::text($name, $value, $field->attributes) }}
-        --}}
     @endslot
 @endcomponent
