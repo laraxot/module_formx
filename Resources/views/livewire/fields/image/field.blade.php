3b6982c872bdfca38ec181f651e2c144742a8270
Malebestia <malebestia@yahoo.it>
2020-10-15 20:08:34 +0200

    .

--- Resources/views/livewire/fields/image/field.blade.php
+++ Resources/views/livewire/fields/image/field.blade.php
@@ -1,1 +1,19 @@
-to do [image]
+@php
+    //.....................................WIP...................................
+    //dddx(get_defined_vars());
+    $model = $_instance->model;
+    //dddx($model);
+@endphp
+
+<div class="form-group row">
+    <label for="{{ $field->name }}" class="col-md-2 col-form-label text-md-right">
+        {{ $field->label }}
+    </label>
+    <div class="col-md-3">
+        <div id="holder" style="margin-top:1px;max-height:150px;">
+            <img class="card-img" src="{{ $model->image_src }}">
+        </div>
+    </div>
+
+</div>
+
