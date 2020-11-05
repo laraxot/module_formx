e699d11a2894ad97879093e123185d0873276a23
marco76tv <marco.sottana@gmail.com>
2020-11-05 11:27:17 +0100

    .

--- Resources/views/includes/components/input/multi_checkbox/field.blade.php
+++ Resources/views/includes/components/input/multi_checkbox/field.blade.php
@@ -12,8 +12,11 @@ $rows=$model->$name();
 $val=$rows->get();
 $related=$rows->getRelated();
 $_panel=Panel::get($related);
+/*
 $_panel->setRows($related);
 $all=$_panel->options();
+*/
+$all=$related->all();
 //ddd($rows);
 //$all=$model->{'all_'.$name};
 //$model_linked=Theme::xotModel(Str::singular($name));
@@ -21,49 +24,51 @@ $all=$_panel->options();
 //$all=$model_linked->get();
 //ddd($_panel);
 @endphp
-<fieldset class="form-group container-fluid border p-2"  >
-    <legend class="col-form-label col-sm-2 pt-0 w-auto"><h4>{{ $name }}</h4></legend>
+<fieldset class="form-group container-fluid border p-2">
+    <legend class="col-form-label col-sm-2 pt-0 w-auto">
+        <h4>{{ $name }}</h4>
+    </legend>
 
-<div class="row" style="border:1px solid  dark;">
-	<div class="col-sm-12">
-		<p>{{ trans('lu::help.nota_multiselect') }}</p>
-	</div>
-	<br style="clear:both"/>
-	<div class="col-sm-5">
-		<select name="{{$name}}[from][]" id="multiselect" class="form-control" size="8" multiple="multiple">
-			@foreach($all as $k => $v)
-			{{--
+    <div class="row" style="border:1px solid  dark;">
+        <div class="col-sm-12">
+            <p>{{ trans('lu::help.nota_multiselect') }}</p>
+        </div>
+        <br style="clear:both" />
+        <div class="col-sm-5">
+            <select name="{{$name}}[from][]" id="multiselect{{$name}}" class="form-control multiselect" size="8" multiple="multiple">
+                @foreach($all as $k => $v)
+                {{--
 			<option value="{{ $v->opt_key }}" >{{ $v->opt_label }}</option>
-			--}}
-			<option value="{{ $_panel->optionId($v) }}" >{{ $_panel->optionLabel($v) }}</option>
-			@endforeach
-		</select>
-	</div>
-	<div class="col-sm-2">
-		<button type="button" id="multiselect_rightAll" class="btn btn-block">
-		<i class="fas fa-angle-double-right"></i>
-		</button>
-		<button type="button" id="multiselect_rightSelected" class="btn btn-block">
-		<i class="fas fa-chevron-right"></i>
-		</button>
-		<button type="button" id="multiselect_leftSelected" class="btn btn-block">
-		<i class="fas fa-chevron-left"></i>
-		</button>
-		<button type="button" id="multiselect_leftAll" class="btn btn-block">
-		<i class="fas fa-angle-double-left"></i>
-		</button>
-	</div>
-	<div class="col-sm-5">
-		<select name="{{$name}}[to][]" id="multiselect_to" class="form-control" size="8" multiple="multiple">
-			@foreach($val as $k => $v)
-			{{--
+                --}}
+                <option value="{{ $_panel->optionId($v) }}">{{ $_panel->optionLabel($v) }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-sm-2">
+            <button type="button" id="multiselect{{$name}}_rightAll" class="btn btn-block">
+                <i class="fas fa-angle-double-right"></i>
+            </button>
+            <button type="button" id="multiselect{{$name}}_rightSelected" class="btn btn-block">
+                <i class="fas fa-chevron-right"></i>
+            </button>
+            <button type="button" id="multiselect{{$name}}_leftSelected" class="btn btn-block">
+                <i class="fas fa-chevron-left"></i>
+            </button>
+            <button type="button" id="multiselect{{$name}}_leftAll" class="btn btn-block">
+                <i class="fas fa-angle-double-left"></i>
+            </button>
+        </div>
+        <div class="col-sm-5">
+            <select name="{{$name}}[to][]" id="multiselect{{$name}}_to" class="form-control" size="8" multiple="multiple">
+                @foreach($val as $k => $v)
+                {{--
 			<option value="{{ $v->opt_key }}" >{{ $v->opt_label }}</option>
-			--}}
-			<option value="{{ $_panel->optionId($v) }}" >{{ $_panel->optionLabel($v) }}</option>
-			@endforeach
-		</select>
-	</div>
-</div>
+                --}}
+                <option value="{{ $_panel->optionId($v) }}">{{ $_panel->optionLabel($v) }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
 </fieldset>
 {{--
 @push('scripts')
@@ -74,3 +79,15 @@ $all=$_panel->options();
 </script>
 @endpush
 --}}
+
+@push('scripts')
+<script type="text/javascript">
+    //jQuery is not a function
+    //jQuery(document).ready(function($) {
+    $(function() {
+        $('.multiselect').multiselect();
+
+    });
+
+</script>
+@endpush
