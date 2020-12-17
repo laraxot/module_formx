26e513d464aff83d950b87b32086685b7983f9ac
Malebestia <malebestia@yahoo.it>
2020-12-17 20:48:07 +0100

    .

--- Resources/views/includes/components/input/docs/field.blade.php
+++ Resources/views/includes/components/input/docs/field.blade.php
@@ -3,7 +3,7 @@
     $src=Form::getValueAttribute($name);
 	if($src=='') $src=asset('/img/nophoto.jpg');
     //Theme::add('/dist/laravel-filemanager/js/stand-alone-button.js');
-    Theme::add($comp_ns.'/js/uploadimgv2.js');
+    Theme::add($comp_ns.'/js/uploadimgv_docs.js');
 	$field->attributes['id']='post_image_src';
 	//dddx(get_defined_vars());
 @endphp
@@ -14,11 +14,28 @@
     @slot('input')
     <div class="card mb-3" >
 		<div class="row no-gutters">
-			<div class="col-md-3">
-                <div id="holder" style="margin-top:1px;max-height:150px;">
-                    <img class="card-img" src="{{ $src }}">
-                </div>
-			</div>
+
+
+
+
+
+
+ <div class="input-group">
+   <span class="input-group-btn">
+        <a id="lfm" data-input="{{ $field->attributes['id'] }}"
+            data-preview="holder" class="btn btn-primary text-white">
+                <i class="fa fa-picture-o"></i> 
+                @lang('formx::txt.select')
+        </a>
+   </span>
+   {{ Form::text($name, $value, $field->attributes) }}
+ </div>
+ <img id="holder" style="margin-top:15px;max-height:100px;">
+
+
+
+
+            {{-- 
 			<div class="col-md-9">
 				<div class="card-body">
 					<div class="input-group">
@@ -32,7 +49,11 @@
 						{{ Form::text($name, $value, $field->attributes) }}
  					</div>
 				</div>
-			</div>
+            </div>
+             --}}
+
+
+            
 		</div>
 	</div>
 
