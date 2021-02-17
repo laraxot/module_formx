5c9a0ed2d7477f9b31a9a1ef29c4feed90fadb26
marco76tv <marco.sottana@gmail.com>
2021-02-17 19:40:50 +0100

    .

---
+++ Macros/BtnIndexAttach.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\FormX\Macros;
+
+//use Illuminate\Http\Request;
+
+//----- services -----
+//--- BASE ---
+
+/**
+ * Class BtnIndexAttach
+ * @package Modules\FormX\Macros
+ */
+class BtnIndexAttach extends BaseFormBtnMacro {
+    /**
+     * @return \Closure
+     */
+    public function __invoke() {
+        return function ($extra) {
+            $class = __CLASS__;
+            $vars = $class::before($extra);
+            if ($vars['error']) {
+                return $vars['error_msg'];
+            }
+
+            return $vars['btn'];
+        }; //end function
+    }
+
+    //end invoke
+}//end class
---
+++ Resources/views/collective/fields/array/field.blade.php
@@ -0,0 +1,22 @@
+TO DO ARRAY FIELD
+{{--
+@php
+	$field=transFields(get_defined_vars());
+@endphp
+@component($blade_component,get_defined_vars())
+	@slot('label')
+		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
+	@endslot
+	@slot('input')
+		{{ Form::text($name, $value, $field->attributes) }} 
+	@endslot
+@endcomponent
+--}}
+@php
+	//ddd(get_defined_vars());
+	// $comp_ns = formx::includes.components.input.array
+	Theme::add($comp_ns.'/js/boh.js');
+	//Theme::add($comp_ns.'/css/boh.css');
+@endphp
+
+	
