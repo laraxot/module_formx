6fead045f783eb3d8afb943fabb20b871638ab46
marco76tv <marco.sottana@gmail.com>
2021-03-10 13:38:44 +0100

    .

--- Resources/views/collective/fields/cell/freeze_label.blade.php
+++ Resources/views/collective/fields/cell/freeze_label.blade.php
@@ -1,8 +1,8 @@
 @php
 foreach ($field->fields as $k => $v) {
     //try{
-    echo '<div class="text-nowrap" >';
-    echo $v->name . ' : ';
+    echo '<div class="text-nowrap" style="border-bottom:1px darkgray;">';
+    echo $v->label ?? $v->name . ' : ';
     echo Theme::inputFreeze(['row' => $row, 'field' => $v]);
     echo '</div>';
     //}catch(\Exception $e){
