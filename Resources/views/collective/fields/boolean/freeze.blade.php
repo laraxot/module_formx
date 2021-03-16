822e32ef64cc026517667bd0dd0b6ed550b30a3e
marco76tv <marco.sottana@gmail.com>
2021-03-16 12:20:06 +0100

    .

--- Resources/views/collective/fields/boolean/freeze.blade.php
+++ Resources/views/collective/fields/boolean/freeze.blade.php
@@ -1,5 +1,5 @@
-@if($field->value)
-<i class="text-info fa fa-lg fa-check"></i>
+@if ($field->value)
+    <i class="text-info fa fa-lg fa-check"></i>
 @else
-<i class="text-danger fa fa-lg fa-times"></i>
+    <i class="text-danger fa fa-lg fa-times"></i>
 @endif
