246437a7de71ccdf92c755af3cb6d0c204cc615a
Malebestia <malebestia@yahoo.it>
2021-06-07 20:14:26 +0200

    .

--- Resources/views/collective/fields/wysiwyg/field.blade.php
+++ Resources/views/collective/fields/wysiwyg/field.blade.php
@@ -1,19 +1,19 @@
 @php
-	//funzionante, da mettere in documentazione!!!
+//funzionante, da mettere in documentazione!!!
 
-	//ps: però come si installa???
+//ps: però come si installa???
 
-	Theme::add('formx::dist/js/app.js');
-	Theme::add('formx::dist/css/app.css');
-	$field=transFields(get_defined_vars());
-	$field->attributes['class']='form-control tinymce'
+Theme::add('formx::dist/js/app.js');
+Theme::add('formx::dist/css/app.css');
+$field = transFields(get_defined_vars());
+$field->attributes['class'] = 'form-control tinymce';
 @endphp
-@component($blade_component,get_defined_vars())
-	@slot('label')
-		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
-	@endslot
-	@slot('input')
-		<br/><br/>
-		{{ Form::textarea($name, $value, $field->attributes) }}
-	@endslot
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
+    @endslot
+    @slot('input')
+        <br /><br />
+        {{ Form::textarea($name, $value, $field->attributes) }}
+    @endslot
 @endcomponent
