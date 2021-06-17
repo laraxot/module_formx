b2c75466c7ceba3bd6dceeacb094dada113e988b
Malebestia <malebestia@yahoo.it>
2021-06-17 20:09:56 +0200

    .

--- Resources/views/collective/fields/select/field_relationship_one.blade.php
+++ Resources/views/collective/fields/select/field_relationship_one.blade.php
@@ -1,8 +1,17 @@
 @php
-    $model=Form::getModel();
-    $field=transFields(get_defined_vars());
-    $rows=$model->$name();
-    /*//--- questa collezione sotto mi serve per vedere se le altre relazioni han bisogno di altro
+/*----ESEMPIO---
+(object) [
+    'type' => 'SelectRelationshipOne',
+    'name' => 'articlecat', //nome relazione
+    'comment' => null,
+    'attributes' => ['placeholder' => 'metti quello che vuoi'],
+],
+*/
+
+$model = Form::getModel();
+$field = transFields(get_defined_vars());
+$rows = $model->$name();
+/*//--- questa collezione sotto mi serve per vedere se le altre relazioni han bisogno di altro
     $tmp=collect(get_class_methods($rows))
         ->filter(
             function($item){
@@ -21,33 +30,34 @@
         }
     )->collapse();
     */
-    //dddx($rows->getRelationName());
+//dddx($rows->getRelationName());
 
-    /*
+/*
       "getForeignKeyName" => "valutatore_id"
       "getQualifiedForeignKeyName" => "schede.valutatore_id"
       "getOwnerKeyName" => "id"
       "getQualifiedOwnerKeyName" => "stabi_dirigente.id"
       "getRelationName" => "valutatore"
     */
-    if(method_exists($rows,'getLocalKeyName')){
-        $name1=$rows->getLocalKeyName();
-    }else{
-        $name1=$rows->getForeignKeyName();
-    }
-    //$field->name=$name1;
-    $field->attributes['name']=$name1;
-    $related=$rows->getRelated();
-    $related_panel=Panel::get($related);
-    $opts=$related_panel->optionsSelect();
+if (method_exists($rows, 'getLocalKeyName')) {
+    $name1 = $rows->getLocalKeyName();
+} else {
+    $name1 = $rows->getForeignKeyName();
+}
+//$field->name=$name1;
+$field->attributes['name'] = $name1;
+$related = $rows->getRelated();
+$related_panel = Panel::get($related);
+$opts = $related_panel->optionsSelect();
+//dddx($field->attributes);
 @endphp
 
 
-@component($blade_component,get_defined_vars())
-	@slot('label')
-	{{ Form::label($name1, $field->label , ['class' => 'control-label form-label']) }} {{-- $field->label_attributes  --}}
-	@endslot
-	@slot('input')
-		{{ Form::select($name1,$opts,$value, $field->attributes) }}
-	@endslot
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name1, $field->label, ['class' => 'control-label form-label']) }} {{-- $field->label_attributes --}}
+    @endslot
+    @slot('input')
+        {{ Form::select($name1, $opts, $value, $field->attributes) }}
+    @endslot
 @endcomponent
