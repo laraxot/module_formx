b1f30eb10371a92d61cbfb1fb031585fef2b2d53
Malebestia <malebestia@yahoo.it>
2021-06-20 20:15:16 +0200

    .

--- Resources/views/collective/fields/address/field.blade.php
+++ Resources/views/collective/fields/address/field.blade.php
@@ -1,1 +1,6 @@
-@include('formx::includes.components.input.address.field_algolia')
+@php
+// NON FUNZIONA, forse vecchio componente deprecato?
+@endphp
+
+{{-- @include('formx::includes.components.input.address.field_algolia') --}}
+@include('formx::includes.components.address.field')
