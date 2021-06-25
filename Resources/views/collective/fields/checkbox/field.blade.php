a7c10add07d6b58de6aa3f40eb916c5b29968f73
Malebestia <malebestia@yahoo.it>
2021-06-25 19:47:34 +0200

    .

--- Resources/views/collective/fields/checkbox/field.blade.php
+++ Resources/views/collective/fields/checkbox/field.blade.php
@@ -1,12 +1,12 @@
 @php
-	$field=transFields(get_defined_vars());
+$field = transFields(get_defined_vars());
 @endphp
-@component($blade_component,get_defined_vars())
-	@slot('label')
-		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
-	@endslot
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
+    @endslot
     @slot('input')
-        {{ Form::bsHidden($name,0) }}  {{-- se non selezionato restituisce 0 al posto di null --}}
-        {{ Form::checkbox($name, 1,$value, $field->attributes) }}
-	@endslot
+        {{ Form::bsHidden($name, 0) }} {{-- se non selezionato restituisce 0 al posto di null --}}
+        {{ Form::checkbox($name, 1, $value, $field->attributes) }}
+    @endslot
 @endcomponent
