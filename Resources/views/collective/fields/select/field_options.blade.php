01ed1c4952a7b53e67d42e9073ba21fd4c6d9bbc
Malebestia <malebestia@yahoo.it>
2021-06-29 13:01:44 +0200

    .

--- Resources/views/collective/fields/select/field_options.blade.php
+++ Resources/views/collective/fields/select/field_options.blade.php
@@ -1,18 +1,28 @@
 @php
-	//ddd(get_defined_vars());
+/*
+(object) [
+ 'type' => 'SelectOptions',
+ 'name' => 'mimetype',
+ 'comment' => null,
+ 'attributes' => ['placeholder' => 'Inserisci il tipo file'],
+ 'options' => ['pdf' => 'pdf', 'doc' => 'doc', 'xls' => 'xls', 'xxx' => 'xxx'],
+ 'col_bs_size' => 3,
+],
+*/
 
+//ddd(get_defined_vars());
 
-	$field=transFields(get_defined_vars());
-	$opts=$options['field']->options; 
-	//$field=transFields(get_defined_vars());
-	//ddd($field);
+$field = transFields(get_defined_vars());
+//dddx($field);
+$opts = $options['field']->options;
+//$field=transFields(get_defined_vars());
 @endphp
 
-@component($blade_component,get_defined_vars())
-	@slot('label')
-	{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
-	@endslot
-	@slot('input')
-		{{ Form::select($name,$opts,$value, $field->attributes) }}
-	@endslot
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
+    @endslot
+    @slot('input')
+        {{ Form::select($name, $opts, $value, $field->attributes) }}
+    @endslot
 @endcomponent
