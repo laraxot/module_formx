b4217a8423a6799813672440ac5b907b59640d56
Davide Cavallini <davidecavallini1987@gmail.com>
2021-09-13 12:17:02 +0200

    .

--- Resources/views/collective/fields/text/field_nolabel.blade.php
+++ Resources/views/collective/fields/text/field_nolabel.blade.php
@@ -6,7 +6,7 @@ $field = transFields(get_defined_vars());
 @component($blade_component, get_defined_vars())
 
     @slot('label')
-        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
+        {{-- Form::label($name,$field->label,['class'=>'control-label']) --}}
     @endslot
     @slot('input')
         @php
@@ -15,11 +15,11 @@ $field = transFields(get_defined_vars());
             echo Form::text($name, $value, $field->attributes);
         } catch (\Exception $e) {
             /*
-                                dddx(['field_name'=>$name,
-                                'value'=>$value,
-                                'err'=>$e->getMessage()
-                                ]);
-                                */
+                                        dddx(['field_name'=>$name,
+                                        'value'=>$value,
+                                        'err'=>$e->getMessage()
+                                        ]);
+                                        */
             echo $e->getMessage();
         }
         @endphp
