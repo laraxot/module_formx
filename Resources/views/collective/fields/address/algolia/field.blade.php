ea941e94ad475d57df36ddface037addd9682820
Malebestia <malebestia@yahoo.it>
2021-09-24 12:53:54 +0200

    .

--- Resources/views/collective/fields/address/algolia/field.blade.php
+++ Resources/views/collective/fields/address/algolia/field.blade.php
@@ -1,108 +1,81 @@
-<!-- text input -->
+@php
+    $field=transFields(get_defined_vars());
+    $label_class='control-label';
+    if(isset($attributes['label_class'])){
+        $label_class=$attributes['label_class'];
+    }
+@endphp
+@component($blade_component,get_defined_vars())
+    @slot('label')
+            {{ Form::label($name, $field->label , ['class' => $label_class]) }}
+        @endslot
+	@slot('input')
+		{{ Form::hidden($name, $value) }}
+		<input type="text" data-address="{&quot;field&quot;: &quot;{{$name}}&quot;}"  class="form-control border-0 shadow-0" autocomplete="off" />
+	@endslot
+@endcomponent
+@push('styles')
+{{--
+	leggi questo per cambiare colori, nel dropdown di algolia places
+	https://community.algolia.com/places/documentation.html
+--}}
 
-<?php
+<style>
+.ap-suggestion { color:darkblue; text-align:left; border-bottom: 1px solid #efefef; }
+.ap-address { color:darkgreen; }
+</style>
+@endpush
 
-// the field should work whether or not Laravel attribute casting is used
-if (isset($field['value']) && (is_array($field['value']) || is_object($field['value']))) {
-    $field['value'] = json_encode($field['value']);
-}
+@push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/places.js@1.11.0"></script>
+<script>
+	$(document).ready(function(){
+		window.AlgoliaPlaces = window.AlgoliaPlaces || {};
 
-?>
+		$('[data-address]').each(function(){
 
-<div @include('crud::inc.field_wrapper_attributes') >
-    <label>{!! $field['label'] !!}</label>
-    @include('crud::inc.field_translatable_icon')
-    <input type="hidden" value="{{ old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $field['default'] ?? '' }}" name="{{ $field['name'] }}">
+			var $this      = $(this),
+			$addressConfig = $this.data('address'),
+			$field = $('[name="'+$addressConfig.field+'"]'),
+			$place = places({
+				//appId: '<YOUR_PLACES_APP_ID>',
+    			//apiKey: '<YOUR_PLACES_API_KEY>',
+				container: $this[0],
+				style: true
+			});
+			function clearInput() {
+				if( !$this.val().length ){
+					$field.val('');
+				}
+			}
 
-    @if(isset($field['prefix']) || isset($field['suffix'])) <div class="input-group"> @endif
-        @if(isset($field['prefix'])) <div class="input-group-addon">{!! $field['prefix'] !!}</div> @endif
-        @if(isset($field['store_as_json']) && $field['store_as_json'])
-        <input
-            type="text"
-            data-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['store_as_json']) && $field['store_as_json'] ? 'true' : 'false'}} }"
-            @include('crud::inc.field_attributes')
-        >
-        @else
-        <input
-            type="text"
-            data-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['store_as_json']) && $field['store_as_json'] ? 'true' : 'false'}} }"
-            name="{{ $field['name'] }}"
-            value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
-            @include('crud::inc.field_attributes')
-        >
-        @endif
-        @if(isset($field['suffix'])) <div class="input-group-addon">{!! $field['suffix'] !!}</div> @endif
-    @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
 
-    {{-- HINT --}}
-    @if (isset($field['hint']))
-        <p class="help-block">{!! $field['hint'] !!}</p>
-    @endif
-</div>
 
-{{-- Note: you can use  to only load some CSS/JS once, even though there are multiple instances of it --}}
+			$place.on('change', function(e){
+				console.log(e.suggestion);
+				var result = JSON.parse(JSON.stringify(e.suggestion));
+				$('input[name=lat]').val(result.latlng.lat);
+				$('input[name=lng]').val(result.latlng.lng);
+				$('input[name=city]').val(result.city);
 
-{{-- ########################################## --}}
-{{-- Extra CSS and JS for this particular field --}}
-{{-- If a field type is shown multiple times on a form, the CSS and JS will only be loaded once --}}
-@if ($crud->checkIfFieldIsFirstOfItsType($field))
+				delete(result.highlight); delete(result.hit); delete(result.hitIndex);
+				delete(result.rawAnswer); delete(result.query);
 
-    {{-- FIELD CSS - will be loaded in the after_styles section --}}
-    @push('crud_fields_styles')
-        <style>
-            .ap-input-icon.ap-icon-pin {
-                right: 5px !important; }
-            .ap-input-icon.ap-icon-clear {
-                right: 10px !important; }
-        </style>
-    @endpush
+				$field.val( JSON.stringify(result) );
 
-    {{-- FIELD JS - will be loaded in the after_scripts section --}}
-    @push('crud_fields_scripts')
-    <script src="https://cdn.jsdelivr.net/npm/places.js@1.11.0"></script>
-    <script>
-        jQuery(document).ready(function($){
-            window.AlgoliaPlaces = window.AlgoliaPlaces || {};
+			});
 
-            $('[data-address]').each(function(){
+			$this.on('change blur', clearInput);
+			$place.on('clear', clearInput);
 
-                var $this      = $(this),
-                $addressConfig = $this.data('address'),
-                $field = $('[name="'+$addressConfig.field+'"]'),
-                $place = places({
-                    container: $this[0]
-                });
+			if( $field.val().length ){
+				var existingData = JSON.parse($field.val());
+				$this.val(existingData.value);
+			}
 
-                function clearInput() {
-                    if( !$this.val().length ){
-                        $field.val('');
-                    }
-                }
 
-                if( $addressConfig.full ){
-
-                    $place.on('change', function(e){
-                        var result = JSON.parse(JSON.stringify(e.suggestion));
-                        delete(result.highlight); delete(result.hit); delete(result.hitIndex);
-                        delete(result.rawAnswer); delete(result.query);
-                        $field.val( JSON.stringify(result) );
-                    });
-
-                    $this.on('change blur', clearInput);
-                    $place.on('clear', clearInput);
-
-                    if( $field.val().length ){
-                        var existingData = JSON.parse($field.val());
-                        $this.val(existingData.value);
-                    }
-                }
-
-                window.AlgoliaPlaces[ $addressConfig.field ] = $place;
-            });
-        });
-    </script>
-    @endpush
-
-@endif
-{{-- End of Extra CSS and JS --}}
-{{-- ########################################## --}}
+			window.AlgoliaPlaces[ $addressConfig.field ] = $place;
+		});
+	});
+</script>
+@endpush
