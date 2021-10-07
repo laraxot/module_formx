5bc9d690ad4abf873caa89caf1c5921c33f11d2b
marco76tv <marco.sottana@gmail.com>
2021-10-08 00:40:57 +0200

    .

--- Resources/views/collective/fields/password/field.blade.php
+++ Resources/views/collective/fields/password/field.blade.php
@@ -1,11 +1,11 @@
 @php
-	$field=transFields(get_defined_vars());
+$field = transFields(get_defined_vars());
 @endphp
-@component($blade_component,get_defined_vars())
-	@slot('label')
-		{{ Form::label($name, $field->label , ['class' => 'control-label']) }}
-	@endslot
-	@slot('input')
-		{{ Form::password($name, $field->attributes) }}
-	@endslot
+@component($blade_component, get_defined_vars())
+    @slot('label')
+        {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
+    @endslot
+    @slot('input')
+        {{ Form::password($name, $field->attributes) }}
+    @endslot
 @endcomponent
