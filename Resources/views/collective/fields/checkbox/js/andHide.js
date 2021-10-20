ade81b04b791668e07527d3b48d91e4fe4b274b2
Davide Cavallini <davidecavallini1987@gmail.com>
2021-10-20 10:56:24 +0200

    checkbox si no

--- Resources/views/collective/fields/checkbox/js/andHide.js
+++ Resources/views/collective/fields/checkbox/js/andHide.js
@@ -1,9 +1,8 @@
 //$('.toggle_hide').
-$('.toggle_hide').click(function(){
-            if($(this).prop("checked") == true){
-                $('.to_hide').hide();
-            }
-            else if($(this).prop("checked") == false){
-                $('.to_hide').show();
-            }
-        });
+$('.toggle_hide').click(function () {
+    if ($(this).prop("checked") == true) {
+        $('.to_hide').hide();
+    } else if ($(this).prop("checked") == false) {
+        $('.to_hide').show();
+    }
+});
