fe4dc8106e76af5adf0d4e7c233c0bb3379a3303
marco76tv <marco.sottana@gmail.com>
2021-12-15 14:05:52 +0100

    .

--- Http/Livewire/Manage/PhpArray.php
+++ Http/Livewire/Manage/PhpArray.php
@@ -12,22 +12,24 @@ class PhpArray extends Component {
     use HandlesArrays;
 
     public string $filename;
+
     //public Collection $field;
     public array $form_data = [];
 
     public function mount(/*string $filename*/) {
         // $this->filename = $filename;
-    }
 
-    public function fields() {
-        return [
+        $this->form_data[$this->field->name] = [
             (object) [
-                'name' => 'nonna papera',
+                'name' => 'prova',
+            ],
+            (object) [
+                'name' => 'nonnapapera',
             ],
         ];
     }
 
-    public function render() {
+    public function getFieldProperty() {
         $field = (object) ([
             'key' => '1',
             'name' => 'test',
@@ -58,18 +60,22 @@ class PhpArray extends Component {
             ],
         ]);
 
-        $this->form_data[$field->name] = [
+        return $field;
+    }
+
+    public function fields() {
+        return [
             (object) [
-                'name' => 'prova',
-            ],
-            (object) [
-                'name' => 'nonnapapera',
+                'name' => 'nonna papera',
             ],
         ];
+    }
+
+    public function render() {
         $view = 'formx::livewire.manage.php-array';
         $view_params = [
             'view' => $view,
-            'field' => $field,
+            'field' => $this->field,
         ];
 
         return view()->make($view, $view_params);
