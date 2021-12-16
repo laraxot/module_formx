0e257ede723c60f0380d132a9047864daf3e85f6
marco76tv <marco.sottana@gmail.com>
2021-12-16 16:52:30 +0100

    .

--- Resources/views/livewire/manage/php-array.blade.php
+++ Resources/views/livewire/manage/php-array.blade.php
@@ -4,30 +4,32 @@
     </div>
 
     <div class="col-md">
-        @if(isset($form_data[$field->name]) && $form_data[$field->name])
+        @if (isset($form_data[$field->name]) && $form_data[$field->name])
             <ul class="list-group mb-2">
-                @foreach($form_data[$field->name] as $key => $value)
+                @foreach ($form_data[$field->name] as $key => $value)
                     <div class="list-group-item list-group-item-action p-2">
                         <div class="form-row">
-                              
-                            @foreach($field->array_fields as $array_field)
+
+                            @foreach ($field->array_fields as $array_field)
                                 @include('formx::livewire.array-fields.' . $array_field->type)
                             @endforeach
-                            
+
                             <div class="col-md-auto">
-                                @if($field->array_sortable)
-                                    <button class="btn btn-sm btn-primary" wire:click="arrayMoveUp('{{ $field->name }}', '{{ $key }}')">
+                                @if ($field->array_sortable)
+                                    <button class="btn btn-sm btn-primary"
+                                        wire:click="arrayMoveUp('{{ $field->name }}', '{{ $key }}')">
                                         <i class="fa fa-arrow-up"></i>
                                     </button>
 
-                                    <button class="btn btn-sm btn-primary" wire:click="arrayMoveDown('{{ $field->name }}', '{{ $key }}')">
+                                    <button class="btn btn-sm btn-primary"
+                                        wire:click="arrayMoveDown('{{ $field->name }}', '{{ $key }}')">
                                         <i class="fa fa-arrow-down"></i>
                                     </button>
                                 @endif
 
                                 <button class="btn btn-sm btn-danger"
-                                        onclick="confirm('{{ __('Are you sure?') }}') || event.stopImmediatePropagation();"
-                                        wire:click="arrayRemove('{{ $field->name }}', '{{ $key }}')">
+                                    onclick="confirm('{{ __('Are you sure?') }}') || event.stopImmediatePropagation();"
+                                    wire:click="arrayRemove('{{ $field->name }}', '{{ $key }}')">
                                     <i class="fa fa-trash-alt"></i>
                                 </button>
                             </div>
@@ -39,6 +41,9 @@
 
         <button class="btn btn-primary" wire:click="arrayAdd('{{ $field->name }}')">
             Add {{ Str::singular($field->label) }}
+        </button>
+        <button class="btn btn-primary" wire:click="save('{{ $field->name }}')">
+            Save
         </button>
 
         @include('formx::livewire.fields.error-help')
